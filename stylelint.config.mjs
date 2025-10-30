934962e6435afea2e9fe4cfb36c3548d73d0298a
mugbee <mugbee2025@gmail.com>
2025-10-31 01:59:21 +0900

    chore: setup Stylelint

---
+++ stylelint.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import("stylelint").Config} */
+export default {
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-standard-scss",
+    "stylelint-config-recess-order",
+    "stylelint-config-html",
+    "stylelint-config-html/astro",
+    "stylelint-config-prettier-scss",
+  ],
+  plugins: [],
+  rules: {},
+  overrides: [],
+};
