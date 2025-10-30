bb8d3009b08793185a7310baec41137a05219586
mugbee <mugbee2025@gmail.com>
2025-10-31 01:06:09 +0900

    chore: setup Prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,22 @@
+/** @type {import("prettier").Config & import("prettier-plugin-astro").PluginOptions} */
+const config = {
+  // 基本設定
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: "as-needed",
+  trailingComma: "es5",
+  bracketSpacing: true,
+  arrowParens: "always",
+  endOfLine: "lf",
+
+  // Astro用プラグイン
+  plugins: ["prettier-plugin-astro"],
+
+  // ファイルタイプ別の設定上書き
+  overrides: [],
+};
+
+export default config;
