ca4ea385232964765c548fedd80ead70779fb2ec
mugbee <mugbee2025@gmail.com>
2025-10-31 23:35:19 +0900

    chore: add GTM

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 // @ts-check
 
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import { defineConfig } from "astro/config";
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mugbee2025.github.io",
-  integrations: [mdx(), sitemap()],
+  site: 'https://mugbee2025.github.io',
+  integrations: [mdx(), sitemap(), partytown()],
   // WSL2でのホットリロード対応
   vite: {
     server: {
