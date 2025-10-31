a15bf4ccf5f2ff8100132fca8be08e2b36fc09bd
mugbee <mugbee2025@gmail.com>
2025-11-01 01:10:43 +0900

    chore: stylelint rule

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,14 +1,17 @@
 /** @type {import("stylelint").Config} */
 export default {
   extends: [
-    "stylelint-config-standard",
-    "stylelint-config-standard-scss",
-    "stylelint-config-recess-order",
-    "stylelint-config-html",
-    "stylelint-config-html/astro",
-    "stylelint-config-prettier-scss",
+    'stylelint-config-standard',
+    'stylelint-config-standard-scss',
+    'stylelint-config-recess-order',
+    'stylelint-config-html',
+    'stylelint-config-html/astro',
+    'stylelint-config-prettier-scss',
   ],
   plugins: [],
-  rules: {},
+  rules: {
+    'color-function-alias-notation': 'with-alpha',
+    'color-function-notation': 'modern',
+  },
   overrides: [],
 };
