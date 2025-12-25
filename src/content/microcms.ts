f303d5b3351e14d8e8033f767c689b7bf95baa0d
mugbee <mugbee2025@gmail.com>
2025-12-26 02:35:41 +0900

    環境変数が未設定時にエラーメッセージを出すようにする

--- src/content/microcms.ts
+++ src/content/microcms.ts
@@ -1,8 +1,16 @@
 import { createClient } from 'microcms-js-sdk';
 
+const { MICROCMS_SERVICE_DOMAIN, MICROCMS_API_KEY } = import.meta.env;
+
+if (!MICROCMS_SERVICE_DOMAIN || !MICROCMS_API_KEY) {
+  throw new Error(
+    'microCMSの環境変数(MICROCMS_SERVICE_DOMAIN, MICROCMS_API_KEY)が設定されていません。'
+  );
+}
+
 const client = createClient({
-  serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
-  apiKey: import.meta.env.MICROCMS_API_KEY,
+  serviceDomain: MICROCMS_SERVICE_DOMAIN,
+  apiKey: MICROCMS_API_KEY,
 });
 
 export const createMicroCMSLoader = (endpoint: string) => {
